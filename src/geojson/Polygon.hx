c7f2048a65d5f83acbcb0f12bc1f1b9f18a58367
kevinresol <kevinresol@gmail.com>
2016-07-30 13:18:07 +0800

    Helper for constructing a regular polygon

--- src/geojson/Polygon.hx
+++ src/geojson/Polygon.hx
@@ -5,6 +5,9 @@ import geojson.util.*;
 private typedef Object = GeoJson<Polygon, Array<Line>>;
 abstract Polygon(Object) from Object to Object {
 	
+	static inline var TO_RADIANS = 3.14159265359 / 180;
+	static inline var TO_DEGREES = 180 / 3.14159265359;
+	
 	public var lines(get, set):Array<Line>;
 	public var type(get, never):GeometryType<Polygon>;
 	
@@ -13,6 +16,37 @@ abstract Polygon(Object) from Object to Object {
 			type: Polygon,
 			coordinates: lines,
 		}
+		
+	/**
+		Construct a regular polygon
+		@param center center of the Polygon
+		@param sides number of sides of the polygon
+		@param radius normalized radius against the sphere's radius. For the Earth, use X(km) / 6378.1 (the Earth has radius of 6378.1km)
+		@param bearing the starting bearing in degrees, default 0
+		
+		Example: 
+		The following code constructs a 10km hexagon on the Earth, somewhere centered in Asia
+		`regular([114.153775, 22.413735], 6, 10 / 6378.1)`
+	**/
+	public static function regular(center:Coordinates, sides:Int, radius:Float, bearingOffset:Float = 0) {
+		var lat = center.latitude * TO_RADIANS;
+		var long = center.longitude * TO_RADIANS;
+		var sinLat = Math.sin(lat);
+		var cosLat = Math.cos(lat);
+		var sinRadius = Math.sin(radius);
+		var cosRadius = Math.cos(radius);
+		bearingOffset *= TO_RADIANS;
+		
+		var points = [for(n in 0...sides) {
+			var bearing = 2 * 3.14159265359 / sides * n + bearingOffset;
+			new Coordinates(
+				(Math.asin(sinLat * cosRadius + cosLat * sinRadius * Math.cos(bearing))) * TO_DEGREES,
+				(long + Math.atan2(Math.sin(bearing) * sinRadius * cosLat, cosRadius - sinLat * sinLat)) * TO_DEGREES
+			);
+		}];
+		points.push(points[0]); // back to origin
+		return new Polygon([points]);
+	}
 		
 	inline function get_lines() return this.coordinates;
 	inline function set_lines(v) return this.coordinates = v;
