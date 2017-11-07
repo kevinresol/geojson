ea1bcc2cba5f51fe2417d27e0a0ad8d82475f330
Kevin Leung <kevinresol@gmail.com>
2017-11-07 15:46:30 +0800

    getter/setter

--- src/geojson/util/Liness.hx
+++ src/geojson/util/Liness.hx
@@ -5,4 +5,12 @@ abstract Liness(Array<Lines>) from Array<Lines> to Array<Lines> from Array<Array
 	@:from
 	public static function fromCoordinates(v:Array<Array<Array<Coordinates>>>):Liness
 		return cast v;
+	
+	public var lines(get, set):Array<Lines>;
+	
+	public inline function new(lines:Array<Lines>)
+		this = lines;
+		
+	inline function get_lines() return this;
+	inline function set_lines(v) return this = v;
 }
