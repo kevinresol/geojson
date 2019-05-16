eda662be761f5c965fc1bf9c861b4b91fae01b49
Kevin Leung <kevinresol@gmail.com>
2019-05-16 15:30:43 +0800

    Minor

--- src/geojson/Feature.hx
+++ src/geojson/Feature.hx
@@ -4,20 +4,21 @@ import haxe.DynamicAccess;
 
 @:forward(id, geometry, properties)
 abstract Feature<T:Geometry.Typed<T>>(FeatureObject<T>) from FeatureObject<T> to FeatureObject<T> {
-	public inline function new(geometry, ?id, ?properties)
+	public inline function new(geometry, ?properties:DynamicAccess<Any>, ?id:String) {
 		this = {
 			type: 'Feature',
-			id: id,
 			geometry: geometry,
 			properties: properties,
 		}
+		if(id != null) this.id = id;
+	}
 		
 	public inline function get() return GeometryCollection.get(this.geometry);
 }
 
 private typedef FeatureObject<T:Geometry.Typed<T>> = {
 	type:String,
-	id:String,
 	geometry:T,
-	properties:DynamicAccess<String>,
+	properties:DynamicAccess<Any>,
+	?id:String,
 }
